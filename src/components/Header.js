073c0b7fdba10b23697f6e8649f9351e3244e80a
Ho Yin Cheng <ho.yin.cheng@lovescomputers.com>
2020-05-21 16:14:35 -0700

    feat: add a header that pulls styles from css module variables

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+      <p>Stackline</p>
+    </header>
+  );
+}
+
+export default Header;
