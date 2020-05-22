acbc7bebf5c32c3d625fd38643b08d3cbed2636b
Ho Yin Cheng <ho.yin.cheng@lovescomputers.com>
2020-05-22 00:03:30 -0700

    feat: product sales page navigation

--- src/components/ProductNav.js
+++ src/components/ProductNav.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faChartBar } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './ProductNav.module.css';
 
 function ProductNav(props) {
+  const { productId } = props.match.params;
+
   return (
-    <nav>
+    <nav className={styles.nav}>
       <ul>
-        <li>OVERVIEW</li>
-        <li>SALES</li>
+        <li>
+          <NavLink
+            to={`/product/${productId}`}
+            exact
+            activeClassName={styles.selected}
+          >
+            <FontAwesomeIcon icon={faHome} />
+            OVERVIEW
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={`/product/${productId}/sales`}
+            exact
+            activeClassName={styles.selected}
+          >
+            <FontAwesomeIcon icon={faChartBar} />
+            SALES
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
 }
 
-export default ProductNav;
+export default withRouter(ProductNav);
