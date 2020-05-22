02d23810e55792bbfcea14ce29c22884f7c1cb47
Ho Yin Cheng <ho.yin.cheng@lovescomputers.com>
2020-05-22 00:23:31 -0700

    Updates

---
+++ precache-manifest.a8134981720f70871f4f915834dcf507.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04dece702c407260fbd7b57e23d3a1a0",
+    "url": "/stackline-assessment/index.html"
+  },
+  {
+    "revision": "6c31c964aab1f592d42a",
+    "url": "/stackline-assessment/static/css/main.0108a4f4.chunk.css"
+  },
+  {
+    "revision": "31852a74a006dcc5482e",
+    "url": "/stackline-assessment/static/js/2.bb84ea21.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/stackline-assessment/static/js/2.bb84ea21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c31c964aab1f592d42a",
+    "url": "/stackline-assessment/static/js/main.6e201ea0.chunk.js"
+  },
+  {
+    "revision": "dfd679040e768621dc5c",
+    "url": "/stackline-assessment/static/js/runtime-main.137e5223.js"
+  }
+]);
