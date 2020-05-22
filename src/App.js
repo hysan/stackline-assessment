e4c52db8870a4a1bbfee81ffac6794bda96c8c2d
Ho Yin Cheng <ho.yin.cheng@lovescomputers.com>
2020-05-21 18:56:54 -0700

    update: refactor fetch logic to be in home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import styles from './App.module.css';
 
@@ -8,45 +7,22 @@ import Home from './pages/Home';
 import Overview from './pages/Overview';
 import Sales from './pages/Sales';
 
-import { getProducts } from './actions/app';
-
-class App extends Component {
-  componentDidMount() {
-    this.props.getProducts();
-  }
-
-  render() {
-    const { loadingProducts } = this.props;
-
-    return (
-      <div className={styles.app}>
-        <Header />
-        {/* Nothing fancy for now as there is no spec for anything but the Sales page */}
-        {loadingProducts ? (
-          <p>Loading...</p>
-        ) : (
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/product/:productId" component={Overview} />
-            <Route exact path="/product/:productId/sales" component={Sales} />
-            <Route path="*">
-              <Redirect to="/" />
-            </Route>
-          </Switch>
-        )}
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className={styles.app}>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/product/:productId" component={Overview} />
+        <Route exact path="/product/:productId/sales" component={Sales} />
+        <Route path="*">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  loadingProducts: state.app.loadingProducts,
-});
-
-const mapDispatchToProps = {
-  getProducts,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-function Home({ products }) {
-  return (
-    <div>
-      <h2>Product Listing</h2>
-      <ul>
-        {products.map(({ id, title }) => (
-          <li key={id}>
-            {title} | <Link to={`/product/${id}`}>Overview</Link> |{' '}
-            <Link to={`/product/${id}/sales`}>Sales</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+import { getProducts } from '../actions/app';
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.getProducts();
+  }
+
+  render() {
+    const { products, loadingProducts } = this.props;
+
+    return (
+      <div>
+        <h2>Product Listing</h2>
+        {/* Nothing fancy for now as there is no spec for anything but the Sales page */}
+        {loadingProducts ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            {products.map(({ id, title }) => (
+              <li key={id}>
+                {title}
+                {' | '}
+                <Link to={`/product/${id}`}>Overview</Link>
+                {' | '}
+                <Link to={`/product/${id}/sales`}>Sales</Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
   products: state.app.products,
+  loadingProducts: state.app.loadingProducts,
 });
 
-export default connect(mapStateToProps)(Home);
+const mapDispatchToProps = {
+  getProducts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
